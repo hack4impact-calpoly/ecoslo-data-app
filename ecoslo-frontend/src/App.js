dafd9625c141e7d0d1c5fa5189cb40d31289292f
bglossner <benglossner@gmail.com>
2020-01-12 14:53:38 -0800

    file1

--- ecoslo-frontend/src/App.js
+++ ecoslo-frontend/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
+import {
+    Redirect,
+    Route,
+    BrowserRouter as Router,
+    Switch
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    
+                </Route>
+                <Route path="/login">
+                    
+                </Route>
+                <Route path="/register">
+
+                </Route>
+                <Route path="/home">
+
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
