36b7f1b28332f0c94a4f41dc1a49e6819b19bba3
Anna Reid <annareid@Annas-MacBook-Pro.local>
2020-01-20 15:10:37 -0800

    added update and view pages and started forms on them

--- ecoslo-frontend/src/Pages/AddEvent.js
+++ ecoslo-frontend/src/Pages/AddEvent.js
@@ -1,10 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
-class AddEvent extends Component {
+class AddEvent extends React.Component {
   render() {
     return (
       <div>
-        <h2>Add Event Page !</h2>
+        <Container>
+        <Form>
+        <Form.Group controlId="formBasicEmail">
+        <Form.Label>Date</Form.Label>
+        <Form.Control placeholder="Enter Date" />
+        <Form.Label>Location</Form.Label>
+        <Form.Control as="select">
+
+        <option>Choose...</option>
+        </Form.Control>
+      </Form.Group>
+        </Form>
+        </Container>
       </div>
     );
   }
