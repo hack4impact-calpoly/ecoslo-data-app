2c3ae025cb4b228810b9ef2765bfb9d4e9f23ee3
Anna Reid <annareid@Annas-MacBook-Pro.local>
2020-01-26 13:00:53 -0800

    final header

--- ecoslo-frontend/src/Components/header.js
+++ ecoslo-frontend/src/Components/header.js
@@ -1,31 +1,37 @@
-import Navbar from 'react-bootstrap/Navbar'
-import "../styles/home.css";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {Redirect} from 'react-router-dom';
+import "../styles/header.css";
 import "../styles/index.css";
 
+
 class Header extends React.Component {
+
     render(){
         return(
             <body>
-            <Navbar bg="dark" variant="dark" className="center-strip">
+            <Navbar className="header-strip" variant="dark">
             <img
-              src="../images/ECOSLO_logo.png"
-              width="30"
-              height="30"
+              src={require("../images/circle-cropped-ecoslo.png")}
+              width="40"
+              height="40"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
             />
-            <Navbar.Brand href="#home">EcoSlo Beach Cleanup</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#add">Add</Nav.Link>
-              <Nav.Link href="#update">Update</Nav.Link>
-              <Nav.Link href="#view">View</Nav.Link>
+            <Nav>
+              <Navbar.Brand href="/home" className = "ml-3">Home</Navbar.Brand>
+              <Nav>
+              <Nav.Link href="/add">Add</Nav.Link>
+              <Nav.Link href="/view">View</Nav.Link>
+              <Nav.Link href="/update">Update</Nav.Link>
+              </Nav>
             </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            </Form>
           </Navbar>
-          </body>
+          </body> 
         )
 
     }
 }
+export default Header;
