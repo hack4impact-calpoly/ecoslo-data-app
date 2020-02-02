ee77a4403889484948285bd570cffcda9924eded
ethana56 <ethanarigil56@gmail.com>
2020-02-02 12:35:00 -0800

    add to database

---
+++ backend/errors.js
@@ -0,0 +1,3 @@
+module.exports = error = Object.freeze({
+    badData: 'badData'
+})
--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,31 @@
 const Express = require('express');
+const AppError = require('./errors');
+const {Pool, Client} = require('pg');
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database
+})
+
+const client = new Client();
+
 const app = Express();
+app.use(Express.json());
+client.connect();
+
 
 app.get('/', (req, res) => {
 	res.send("Hello world");
+})
+
+app.post('/add', (req, res) => {
+	/* TODO: authentication */
+	if (!authenticateInput(req.body.item)) {
+		res.status(400).send(AppError.badData);
+		return;
+	}
+	
 })
 
 app.listen(8000);
