d6e9b171e5ae6668c8acceee274d3271c2fb2679
Anna Reid <annareid@Annas-MacBook-Pro-5.local>
2020-03-08 12:57:25 -0700

    remove extra update cols

--- ecoslo-frontend/src/Pages/Update.js
+++ ecoslo-frontend/src/Pages/Update.js
@@ -3,6 +3,7 @@ import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import {Row, Col} from 'react-bootstrap';
 import Table from 'react-bootstrap/Table'
 window.$field_count = 1;
 
@@ -11,7 +12,7 @@ let counter=1;
 class Update extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {inputs: ['1'],
+    this.state = {inputs: ["1"],
      cols: {Cigarette_Butts: '', Food_Wrappers: '', Plastic_Take_Out_Containers: '', Plastic_Bottle_Caps: '', Metal_Bottle_Caps: '',Plastic_Lids: '', Straws_And_Stirrers: '', 
      Forks_Knives_And_Spoons: '', Plastic_Beverage_Bottles: '', Glass_Beverage_Bottles: '', Beverage_Cans: '', Plastic_Grocery_Bags: '', Other_Plastic_Bags: '', Paper_Bags: '', 
      Paper_Cups_And_Plates: '', Plastic_Cups_And_Plates: '', Foam_Cups_And_Plates: '', Fishing_Buoys_Pots_And_Traps: '', Fishing_Net_And_Pieces: '', Fishing_Line: '', Rope: '', 
@@ -26,14 +27,24 @@ class Update extends React.Component {
     //this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+
+  handleRemove(event, index) {
+    var list_remove = this.state.inputs;
+    var list_remove_vals = this.state.input_vals;
+    console.log(list_remove);
+    console.log(index);
+    list_remove.splice(index, 1)
+    list_remove_vals.splice(index, 1)
+    console.log(list_remove);
+    this.setState({inputs: list_remove});
+    this.setState({input_vals: list_remove_vals});
+
+  }
+
   
   handleChangeTextBox(event, index) {
-    console.log(index);
-    console.log(index);
-    console.log(this.state.input_vals);
     var new_list_input = this.state.input_vals;
     new_list_input[index][1] = event.target.value
-    console.log(index)
     this.setState({input_vals: new_list_input});
   }
 
@@ -76,64 +87,77 @@ class Update extends React.Component {
         </Form.Control>
       {
         this.state.inputs.map((value, index) => {
-          let curr_field_count = index
-          console.log('cfc: ', curr_field_count)
           return (
             <div>
         <Form.Label>Item</Form.Label>
-        <Form.Control name={index.toString()} as="select"  onChange={(e) => this.handleChangeDropDown(e, index)}>
-          <option>Choose...</option>
-          <option>Cigarette_Butts</option>
-          <option>Food_Wrappers</option>
-          <option>Plastic_Take_Out_Containers</option>
-          <option>Plastic_Bottle_Caps</option>
-          <option>Metal_Bottle_Caps</option>
-          <option>Plastic_Lids</option>
-          <option>Straws_And_Stirrers</option>
-          <option>Forks_Knives_And_Spoons</option>
-          <option>Plastic_Beverage_Bottles</option>
-          <option>Glass_Beverage_Bottles</option>
-          <option>Beverage_Cans</option>
-          <option>Plastic_Grocery_Bags</option>
-          <option>Other_Plastic_Bags</option>
-          <option>Paper_Bags</option>
-          <option>Paper_Cups_And_Plates</option>
-          <option>Plastic_Cups_And_Plates</option>
-          <option>Foam_Cups_And_Plates</option>
-          <option>Fishing_Buoys_Pots_And_Traps</option>
-          <option>Fishing_Net_And_Pieces</option>
-          <option>Fishing_Line</option>
-          <option>Rope</option>
-          <option>Six_Pack_Holders</option>
-          <option>Other_Plastic_Or_Foam_Packaging</option>
-          <option>Other_Plastic_Bottles</option>
-          <option>Strapping_Bands</option>
-          <option>Tobacco_Packaging_Or_Wrap</option>
-          <option>Appliances</option>
-          <option>Balloons</option>
-          <option>Cigar_Tips</option>
-          <option>Cigarette_Lighters</option>
-          <option>Construction_Materials</option>
-          <option>Fireworks</option>
-          <option>Tires</option>
-          <option>Condoms</option>
-          <option>Diapers</option>
-          <option>Syringes</option>
-          <option>Tampons</option>
-          <option>Foam_Pieces</option>
-          <option>Glass_Pieces</option>
-          <option>Plastic_Pieces</option>
+        <Form.Row>
+          <Col xs={11}>
+        <Form.Control name={index.toString()} as="select" onChange={(e) => this.handleChangeDropDown(e, index)}>
+            <option>Choose...</option>
+            <option>Cigarette_Butts</option>
+            <option>Food_Wrappers</option>
+            <option>Plastic_Take_Out_Containers</option>
+            <option>Plastic_Bottle_Caps</option>
+            <option>Metal_Bottle_Caps</option>
+            <option>Plastic_Lids</option>
+            <option>Straws_And_Stirrers</option>
+            <option>Forks_Knives_And_Spoons</option>
+            <option>Plastic_Beverage_Bottles</option>
+            <option>Glass_Beverage_Bottles</option>
+            <option>Beverage_Cans</option>
+            <option>Plastic_Grocery_Bags</option>
+            <option>Other_Plastic_Bags</option>
+            <option>Paper_Bags</option>
+            <option>Paper_Cups_And_Plates</option>
+            <option>Plastic_Cups_And_Plates</option>
+            <option>Foam_Cups_And_Plates</option>
+            <option>Fishing_Buoys_Pots_And_Traps</option>
+            <option>Fishing_Net_And_Pieces</option>
+            <option>Fishing_Line</option>
+            <option>Rope</option>
+            <option>Six_Pack_Holders</option>
+            <option>Other_Plastic_Or_Foam_Packaging</option>
+            <option>Other_Plastic_Bottles</option>
+            <option>Strapping_Bands</option>
+            <option>Tobacco_Packaging_Or_Wrap</option>
+            <option>Appliances</option>
+            <option>Balloons</option>
+            <option>Cigar_Tips</option>
+            <option>Cigarette_Lighters</option>
+            <option>Construction_Materials</option>
+            <option>Fireworks</option>
+            <option>Tires</option>
+            <option>Condoms</option>
+            <option>Diapers</option>
+            <option>Syringes</option>
+            <option>Tampons</option>
+            <option>Foam_Pieces</option>
+            <option>Glass_Pieces</option>
+            <option>Plastic_Pieces</option>
         </Form.Control>
+          </Col>
+          <Col>
+            <button type="button" className="close" aria-label="Close" onClick={(e) => {this.handleRemove(e, index)}}>
+            <span aria-hidden="true">&times;</span>
+            </button>
+          </Col>
+        </Form.Row>
 
+
+        <Form.Row>
+          <Col xs={11}>
             <Form.Label>Number</Form.Label>
             <Form.Control placeholder="Enter number" name={index.toString()} value={this.state.input_vals[index][1]} onChange={(e) => this.handleChangeTextBox(e, index)}/>
+          </Col>
+        </Form.Row>
             </div>
           )
         })
       }
+      <Button onClick={(e) => {this.handleAddItem(e)}}>Update Another Item</Button> 
 
 
-        <Button onClick={(e) => {this.handleAddItem(e)}}>Update Another Item</Button> 
+        
 
 
         </Form.Group>
